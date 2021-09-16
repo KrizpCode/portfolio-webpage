c973d3c52f01d9657a733ac540714162d9df0e29
KrizpCode <93johan.e@gmail.com>
2021-09-16 11:57:24 +0200

    Added a simple scrollToTop-button on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,43 @@
 import styled from 'styled-components';
 
+import { elementScrollIntoView } from "seamless-scroll-polyfill";
+
 const FooterSection = styled.footer`
     background: #1B242F;
     color: #FFF;
     heigh: 60px;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    min-height: 40vh;
+    position: relative;
+`
+
+const ToTopLink = styled.a`
+    background: #E31B6D;
+    padding: 10px;
+    text-align: center;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    margin-top: -20px;
 `
 
 const Footer = () => {
+    const scrollToTop = (): void => {
+        const element = document.getElementById(`header`)
+        
+        if (element) {
+            elementScrollIntoView(element, { behavior: "smooth", block: "center", inline: "center" });
+        }
+
+        return
+    }
+
     return (
         <FooterSection>
+            <ToTopLink onClick={() => scrollToTop()}>^</ToTopLink>
             <p>Johan 2021 All Rights Reserved</p>
         </FooterSection>
     )
